548b5cc31d6d9f0cbcb3282418f3f6c020bbb65b
rohitnrl <rohitnrl@gmail.com>
2025-09-29 14:09:38 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,9 @@
 provider "azurerm" {
   version = ">=2.0"
   # The "feature" block is required for AzureRM provider 2.x. 
-  features {}
-}
 
- "azurerm_resource_group" "rg" {
+
+resources "azurerm_resource_group" "rg" {
   name     = "${var.resource-group-name}-${var.environment}"
   location = "westeurope"
   
